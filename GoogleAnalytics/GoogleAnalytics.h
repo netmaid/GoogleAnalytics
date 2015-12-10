2b7d535440dd2897d8307c4c1311c7cf8db432df
Chungmin Ahn <lp90128@linecorp.com>
2015-12-10 15:09:55 +0900

    first commit

---
+++ GoogleAnalytics/GoogleAnalytics.h
@@ -0,0 +1,30 @@
+//
+//  GoogleAnalytics.h
+//  GoogleAnalytics
+//
+//  Created by ened on 2015. 12. 9..
+//  Copyright © 2015년 netmaid. All rights reserved.
+//
+
+#import <GoogleAnalytics/GAI.h>
+#import <GoogleAnalytics/GAIDictionaryBuilder.h>
+#import <GoogleAnalytics/GAIEcommerceFields.h>
+#import <GoogleAnalytics/GAIEcommerceProduct.h>
+#import <GoogleAnalytics/GAIEcommerceProductAction.h>
+#import <GoogleAnalytics/GAIEcommercePromotion.h>
+#import <GoogleAnalytics/GAIFields.h>
+#import <GoogleAnalytics/GAILogger.h>
+#import <GoogleAnalytics/GAITrackedViewController.h>
+#import <GoogleAnalytics/GAITracker.h>
+
+#import <GoogleAnalytics/TAGContainer.h>
+#import <GoogleAnalytics/TAGContainerOpener.h>
+#import <GoogleAnalytics/TAGDataLayer.h>
+#import <GoogleAnalytics/TAGLogger.h>
+#import <GoogleAnalytics/TAGManager.h>
+
+//! Project version number for GoogleAnalytics.
+FOUNDATION_EXPORT double GoogleAnalyticsVersionNumber;
+
+//! Project version string for GoogleAnalytics.
+FOUNDATION_EXPORT const unsigned char GoogleAnalyticsVersionString[];
